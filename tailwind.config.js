175a26b4d826632467b622418c65de678299ae51
gono1045 <tequilasunrise1226@gmail.com>
2025-09-11 22:45:06 +0900

    モーダル登録画面編集前までのMac移行セットアップ

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   darkMode: 'class', // classベースのダークモード
   content: [
@@ -6,14 +8,20 @@ module.exports = {
     './src/main/resources/static/js/**/*.js',    // JS
   ],
   safelist: [
-	'font-title',
-	'font-body',
+  'font-title',
+  'font-body',
+  'dark:text-green-300',
+  'text-green-700',
+  'dark:text-green-300'
   ],
   theme: {
     extend: {
       fontFamily: {
-		title: ['Merienda', 'cursive'], // タイトル用
-		body: ['ui-sans-serif', 'system-ui'], // 本文用は標準
+    title: ['Merienda', 'cursive'], // タイトル用
+    body: ['ui-sans-serif', 'system-ui'], // 本文用は標準
+      },
+      colors: {
+        green: colors.green,
       },
     },
   },
